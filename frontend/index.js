3edd27ec35e049899adf92b7663464cba126453c
Punishing Bird <giahuyluong609@gmail.com>
2026-08-16 21:29:31 +0700

    new

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+window.history.replaceState({},"","/"); 
+// xóa đuôi file 
+
+
+// kt login hay chx = cách kt storage 
+function checkLogin(){
+    const user_storage = localStorage.getItem("user")
+
+}
