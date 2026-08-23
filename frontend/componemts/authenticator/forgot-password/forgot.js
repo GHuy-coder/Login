1eb75d8f0771443581273b19ccc3696b36cf1923
Punishing Bird <giahuyluong609@gmail.com>
2026-08-23 21:10:44 +0700

    trust-me-i-don't-use-AI

--- frontend/componemts/authenticator/forgot-password/forgot.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("listen-button").addEventListener("click", backFunction);
-
-function backFunction() {
-    window.location.href = "../login/login.html"
-}
--- scripts.js
+++ scripts.js
@@ -1,1 +1,1 @@
-window.location.href="frontend/index.html"
+window.location.href="frontend/home/index.html"
